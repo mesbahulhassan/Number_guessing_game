f4d1c9bde04c1d898f0d9490732a5ffce52c0f04
Md. Mesbah Ul Hassan <mesbahulhassan01@gmail.com>
2026-08-28 01:03:55 +0600

    Add files via upload

---
+++ Number_guessing_game.py
@@ -0,0 +1,40 @@
+import random
+low = int(input('input the lower value of the game you want to play: '))
+high = int(input('input the higher value of the game you want to play: '))
+num = random.randint(low, high)
+guesses = int(input("enter the number of guesses you want to have: "))
+while guesses > (high - low):
+    print(f"guesses must be equal to or less than {high - low}")
+    guesses = int(input("enter the number of guesses you want to have: "))
+
+while guesses > 0:
+    try:
+        guess = int(input(f'Guess a number from {low} to {high}: '))
+        while guess > high or guess < low:
+            print(f'Your guess is out of range {low} and {high}')
+            guess = int(input(f'Guess a number from {low} to {high}: '))
+            if guess > num:
+                guesses -= 1
+                print(f"Guess Lower \nRemained attempt {guesses}")
+            
+            elif guess < num:
+                guesses -= 1
+                print(f"Guess Higher \nRemained attempt {guesses}")
+            
+            else:
+                print('You Guessed correctly ')
+                break
+
+    except ValueError:
+        print("Value Error --> Enter a real Integer from this range")
+
+print('Game Ended')
+
+
+
+
+
+
+
+
+#print(num)
